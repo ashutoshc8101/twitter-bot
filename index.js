aa5e5aa81bd81ebb600622e8045de7c5afd09b32
dromestrome <ashutoshc8101@gmail.com>
2018-03-09 22:50:43 +0530

    Basic Functionality added

---
+++ index.js
@@ -0,0 +1,37 @@
+// Importing neccessary modules //
+const twitter = require('twitter')
+const fs = require('fs')
+
+// Configuring and initializing //
+require('dotenv').config()
+
+let client = new twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_KEY,
+    access_token_secret: process.env.ACCESS_SECRET
+})
+
+let count;
+let readStream = fs.createReadStream('count.dat')
+readStream.on('data', (chunck) => {
+    count = chunck.toString('utf-8')
+    
+    setInterval(() => {
+        client.post('statuses/update', { status: `Tweet #${count}` })
+            .then(tweet => {
+                console.log(tweet.text)
+                let stream = fs.createWriteStream('count.dat')
+                stream.once('open', (fd) => {
+                    stream.write(new Buffer(++count))
+                    stream.end()
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, 5000)
+
+})
+
+
